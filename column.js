e3a12ea541834ca6b946e030507c8c9a6e9320d5
KevDas <kevdasrath@gmail.com>
2024-08-26 15:12:58 -0400

    Cleaned up code for script.js and column.js to be easier to read

--- column.js
+++ column.js
@@ -1,49 +1,57 @@
-class Column{
+class Column
+{
 
-    constructor(x,y,width, height){
+    constructor(x,y,width, height)
+    {
         this.x = x;
         this.y = y;
         this.width = width;
         this.height = height;
-        this.queue=[];
+        this.queue= [];
     }
 
 
-    changeHeight(height){
-
-        for (let i = 1; i < 10; ++i){
+    changeHeight(height)
+    {
+        for (let i = 1; i < 10; ++i)
+        {
             this.height = height; 
             const left = this.x - this.width/2;
-        const top = this.y - this.height;
-        const right = this.x + this.width/2;
+            const top = this.y - this.height;
 
-        ctx.beginPath();
-        //sets the fill Style of the rectangle to a gray
-        ctx.fillStyle = "rgb(150, 150, 150)";
-        //creating the rectangle path
-        ctx.rect(left, top, this.width, this.height);
-        //fills the rectangle path specified above with the gray color
-        ctx.fill();
+            ctx.beginPath();
+            //sets the fill Style of the rectangle to a gray
+            ctx.fillStyle = "rgb(150, 150, 150)";
+            //creating the rectangle path
+            ctx.rect(left, top, this.width, this.height);
+            //fills the rectangle path specified above with the gray color
+            ctx.fill();
         }
 
     }
-    moveTo(loc, yOffset = 1, frameCount = 1){
-        for (let i = 1; i <= frameCount; ++i){
+    
+    moveTo(loc, yOffset = 1, frameCount = 1)
+    {
+        for (let i = 1; i <= frameCount; ++i)
+        {
             const t = i/ frameCount;
-
             const u = Math.sin(t * Math.PI);
-            this.queue.push({
-            x:linearInterpolation(this.x, loc.x, t), y:linearInterpolation(this.y, loc.y, t)
-            + u * this.width/2 * yOffset
-           }); 
+            this.queue.push(
+            {
+                x:linearInterpolation(this.x, loc.x, t), 
+                y:linearInterpolation(this.y, loc.y, t) + u * this.width/2 * yOffset
+            }); 
         }
     }
 
-    jump(frameCount = 20){
-        for (let i = 1; i <= frameCount; ++i){
+    jump(frameCount = 20)
+    {
+        for (let i = 1; i <= frameCount; ++i)
+        {
             const t = i / frameCount;
             const u = Math.sin(t * Math.PI);
-            this.queue.push({
+            this.queue.push(
+            {
                 x:this.x, 
                 y: this.y - u * this.width 
             });
@@ -51,22 +59,22 @@ class Column{
     }
 
 
-    draw(ctx, color){
+    draw(ctx, color)
+    {
         let changed = false;
-
         //if there are animations to perform, change the x and y positions of the column
         // and remove the frame from the queue / return that a change is occuring
-        if (this.queue.length > 0){
+        if (this.queue.length > 0)
+        {
             const {x, y} = this.queue.shift();
             this.x = x;
             this.y = y;
             changed = true;
             color = "red";
         }
+
         const left = this.x - this.width/2;
         const top = this.y - this.height;
-        const right = this.x + this.width/2;
-
         //creates a new path where we will draw a rectangle
         ctx.beginPath();
         //sets the fill Style of the rectangle to a gray
@@ -81,6 +89,7 @@ class Column{
     
 }
 
-function linearInterpolation(a, b, t){
+function linearInterpolation(a, b, t)
+{
     return a + (b - a) * t;  
 }
--- script.js
+++ script.js
@@ -1,58 +1,55 @@
 myCanvas.width = 900;
 myCanvas.height = 600;
-
+const ctx = myCanvas.getContext("2d");
 const margin = 30;
+
 let array = [];
-/*const n = 128;*/
 let reset = [];
 let moves = [];
 let cols = [];
-/*let spacing = ((myCanvas.width - 2 * margin) / n);*/
 let heights = [];
-
-
-
-//variable for our canvas that will hold the columns of the graph 
-const ctx = myCanvas.getContext("2d");
-
 const maxColumnHeight = 200;
-
 const fixedValues = [16, 32, 64, 128, 256];
 
-const slider = document.getElementById('myRange');
-const displayValue = document.getElementById('sliderValue');
 
-slider.addEventListener('input', function() {
-    const value = fixedValues[slider.value];
-    displayValue.innerText = value;
-    init(value); // Call your initialization function with the selected value
+
+//Gets value from fixedValues silder and sets the array size == to it, generating a new array of said size and changing
+//text value on screen to reflect change
+document.getElementById('myRange').addEventListener('input', function() 
+{
+    const value = fixedValues[document.getElementById('myRange').value];
+    document.getElementById('sliderValue').innerText = value;
+    init(value); 
 });
 
-function init(n){
+function init(n)
+{
     
     document.getElementById('sliderValue').innerText = n;
     array = [];
-    console.log("Array Length: "+ array.length + " N = " + n);
-    console.log("NEW ARRAY");
-    for (let i = 0; i < n; ++i){
+   
+    for (let i = 0; i < n; ++i)
+    {
         array[i] = Math.floor(Math.random() * (2000 - 400) + 400);
         reset[i] = array[i];
-        console.log(array[i]);
     }
+
     heights = [];
     moves = [];
     cols = [];
-
     array.length = n; 
+
+
+
     //creating n number of columns and initializing their position in the array
-    for (let i = 0; i < array.length; ++i){
+    for (let i = 0; i < array.length; ++i)
+    {
         const x = i * ((myCanvas.width - 2 * margin) / n) + ((myCanvas.width - 2 * margin) / n) / 2 + margin;
         const y = myCanvas.height - margin;
         const width = ((myCanvas.width - 2 * margin) / n) - 4;
         const height =  array[i]/maxColumnHeight * 40;
         //creating the column with the properties specified above 
         cols[i] = new Column(x,y,width,height);
-    
     }
 }
 
@@ -75,9 +72,11 @@ function resetArr(){
     
     }
 }
-function play(n){
-    console.log(array.length);
-    switch(n){
+function play(n)
+{
+    
+    switch(n)
+    {
         case 1: moves = bubbleSort(array);
             break;
         case 2: moves = quickSort(array);
@@ -93,10 +92,6 @@ function play(n){
 }
 
 
-
-
-
-
 animate();
 
 /*----------------------------------BUBBLE SORT---------------------------*/
@@ -104,38 +99,22 @@ function bubbleSort(array){
     const heights = [];
     const moves = [];
     
-    //while at least one variable is being swapped each iteration, repeat the loop
-    do {
-         var swapped = false;
-
-         for (let i = 1; i < array.length; ++i)
-            {
-
-            //if a swap needs to be intiated,
+    do 
+    {
+        var swapped = false;
+        for (let i = 1; i < array.length; ++i)
+        {
             if (array[i - 1] > array[i])
             {
                 swapped = true;
-                //swaps the values of the two values in the actual array 
                 [array[i - 1], array[i]] = [array[i], array[i -1]];
-                //pushes the indices and the swap boolean to the moves array 
                 moves.push(
                     {indices:[i - 1, i], swap:true}
                 );
             }
-
-
-            //otherwise, just push that the indices were not infact swapped
-            /*else 
-            {
-                moves.push(
-                    {indices:[i-1, i], swap:false}
-                );
-            }*/
-
-         }
+        }
     } while(swapped);
 
-      
     return moves;
 }
 /*---------------------------------------------------------------------- */
@@ -146,21 +125,22 @@ function selectionSort(array) {
     const heights = [];
     const moves = [];
     
-    for (let start = 0; start < array.length - 1; ++start) {
+    for (let start = 0; start < array.length - 1; ++start) 
+    {
         let minIndex = start;
-        
-        // Find the index of the minimum element
-        for (let i = start + 1; i < array.length; ++i) {
-            if (array[i] < array[minIndex]) {
+        for (let i = start + 1; i < array.length; ++i) 
+            if (array[i] < array[minIndex]) 
                 minIndex = i;
-            }
-        }
+            
         
-        // Only swap if the minimum is not the current element
-        if (minIndex !== start) {
+        
+        if (minIndex !== start) 
+        {
             [array[start], array[minIndex]] = [array[minIndex], array[start]];
             moves.push({ indices: [start, minIndex], swap: true });
-        } else {
+        } 
+        else 
+        {
             moves.push({ indices: [start, minIndex], swap: false });
         }
     }
@@ -171,24 +151,25 @@ function selectionSort(array) {
 /*---------------------------------------------------------------------- */
 /*-------------------------Insertion Sort--------------------------------*/
 
-function insertionSort(array){
+function insertionSort(array)
+{
     const heights = [];
     const moves = [];
 
-    for (let start = 0; start < array.length - 1; ++start){
-
-        for (let i = start; i >= 0; --i){
-            if (array[i + 1] < array[i]){
+    for (let start = 0; start < array.length - 1; ++start)
+    {
+        for (let i = start; i >= 0; --i)
+        {
+            if (array[i + 1] < array[i])
+            {
                 [array[i + 1], array[i]] = [array[i], array[i + 1]];
                 moves.push({indices: [i, i + 1], swap:true});
             }
-            else {
+            else 
+            {
                 moves.push({indices:[i, i + 1], swap:false});
             }
         }
-
-
-
     }
     return moves;
 }
@@ -198,65 +179,57 @@ function insertionSort(array){
 /*---------------------------------------------------------------------- */
 /*-------------------------Quick Sort--------------------------------*/
 function partition(arr, low, high)
+{
+    let temp;
+    let pivot = arr[high];
+  
+    let i = (low - 1);
+    for (let j = low; j <= high - 1; j++) 
     {
-        let temp;
-        let pivot = arr[high];
-  
-        // index of smaller element
-        let i = (low - 1);
-        for (let j = low; j <= high - 1; j++) {
-  
-            // If current element is
-            // smaller than or
-            // equal to pivot
-            if (arr[j] <= pivot) {
-                i++;
-  
-                // swap arr[i] and arr[j]
-                temp = arr[i];
-                arr[i] = arr[j];
-                arr[j] = temp;
-                moves.push({indices:[i, j], swap:true});
-            }
+        if (arr[j] <= pivot) 
+        {
+            i++;
+            temp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp;
+            moves.push({indices:[i, j], swap:true});
         }
-  
-        // swap arr[i+1] and arr[high]
-        // (or pivot)
-        temp = arr[i + 1];
-        arr[i + 1] = arr[high];
-        arr[high] = temp;
-        moves.push({indices:[i +1, high], swap:true})
-        return i + 1;
     }
+  
+    temp = arr[i + 1];
+    arr[i + 1] = arr[high];
+    arr[high] = temp;
+    moves.push({indices:[i +1, high], swap:true})
+    return i + 1;
+}
 
-    function qSort(arr, low, high)
+function qSort(arr, low, high)
+{
+    if (low < high) 
     {
-        if (low < high) {
-            /* pi is partitioning index, 
-            arr[pi] is now at right place */
-            let pi = partition(arr, low, high);
+        /* pi is partitioning index, 
+        arr[pi] is now at right place */
+        let pi = partition(arr, low, high);
   
-            // Recursively sort elements
-            // before partition and after
-            // partition
-            qSort(arr, low, pi - 1);
-            qSort(arr, pi + 1, high);
-        }
-        return moves; 
+        qSort(arr, low, pi - 1);
+        qSort(arr, pi + 1, high);
     }
 
+    return moves; 
+}
 
-    function quickSort(array){
-        const heights = [];
-        moves =
-        qSort(array, 0, array.length - 1)
 
-        return moves;
-    }
+function quickSort(array)
+{
+    const heights = [];
+    moves = qSort(array, 0, array.length - 1);
+    return moves;
+}
 /*---------------------------------------------------------------------- */
 
 /*-------------------------Merge Sort ITERATIVE--------------------------------*/
-function mergeSortIt(arr) {
+function mergeSortIt(arr) 
+{
     const heights = [];
     var n = arr.length;
     var curr_size;
@@ -264,30 +237,22 @@ function mergeSortIt(arr) {
     const moves = [];
     //create subarrays of that double each time, starting from 1 up to the size of the
     // original array
-    for (curr_size = 1; curr_size <= n - 1; curr_size = 2 * curr_size) {
-
-     
-        for (left_start = 0; left_start < n - 1; left_start += 2 * curr_size) {
-         
+    for (curr_size = 1; curr_size <= n - 1; curr_size = 2 * curr_size) 
+    {
+        for (left_start = 0; left_start < n - 1; left_start += 2 * curr_size) 
+        {
             var mid = Math.min(left_start + curr_size - 1, n - 1);
-
             var right_end = Math.min(left_start + 2 * curr_size - 1, n - 1);
-
-           
             merge(arr, left_start, mid, right_end);
         }
-        
-
-       
     }
     
-
     return moves; 
 }
 
 
-
-function merge(arr , l , m , r) {
+function merge(arr , l , m , r) 
+{
     var i, j, k;
     var n1 = m - l + 1;
     var n2 = r - m;
@@ -295,89 +260,85 @@ function merge(arr , l , m , r) {
     var L = Array(n1).fill(0);
     var R = Array(n2).fill(0);
 
-    
     for (i = 0; i < n1; i++)
         L[i] = arr[l + i];
+
     for (j = 0; j < n2; j++)
         R[j] = arr[m + 1 + j];
-
 
     i = 0;
     j = 0;
     k = l;
-    while (i < n1 && j < n2) {
-        if (L[i] <= R[j]) {
+
+    while (i < n1 && j < n2) 
+    {
+        if (L[i] <= R[j]) 
+        {
             arr[k] = L[i];
             heights.push({index:k, newHeight: L[i]/maxColumnHeight *40});
             i++;
-        } else {
+        } 
+        else 
+        {
             arr[k] = R[j];
             heights.push({index:k, newHeight: R[j]/maxColumnHeight * 40});
-
             j++;
         }
         k++;
     }
 
-    while (i < n1) {
+    while (i < n1) 
+    {
         arr[k] = L[i];
         heights.push({index:k, newHeight: L[i]/maxColumnHeight *40});
-
         i++;
         k++;
     }
 
 
-    while (j < n2) {
+    while (j < n2) 
+    {
         arr[k] = R[j];
         heights.push({index:k, newHeight: R[j]/maxColumnHeight * 40});
-
         j++;
         k++;
     }
-
-    
 }
-
-
-
 /*---------------------------------------------------------------------- */
 
-function animate() {
+function animate() 
+{
 
     //create a transparent rectangular area to populate
     ctx.clearRect(0, 0, myCanvas.width, myCanvas.height);
-
     let changed = false;
-    
-    for (let i = 0; i < cols.length; ++i){
-
+    for (let i = 0; i < cols.length; ++i)
+    {
         //if any changed frame is encountered, changed will be set to true
         changed = cols[i].draw(ctx, "gray")||changed;
-        
-
     }
     
-    
-
-    if (!changed && moves.length > 0){
+    if (!changed && moves.length > 0)
+    {
         const move = moves.shift();
         const [i, j] = move.indices;
         //if move.swap is true, animate the swap
-        if (move.swap){
+        if (move.swap)
+        {
             cols[i].moveTo(cols[j]);
             cols[j].moveTo(cols[i], -1);
             [cols[i], cols[j]] = [cols[j], cols[i]];
-        }
-        
+        }  
     }
 
-    if (!changed && heights.length > 0){
+    if (!changed && heights.length > 0)
+    {
         const h = heights.shift();
         const idx = h.index; 
         const nHeight = h.newHeight;
         cols[idx].height = nHeight;
         cols[idx].jump(2);
     }
+    
     requestAnimationFrame(animate);
 }
